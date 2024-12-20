c316b5c288398b5935150188f7be5d969cfff8cd
ysh1535 <ysh1535>
2024-12-20 18:21:38 +0900

    레이아웃 완성 😁

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('시작하기');
+import { menu } from "./menu.js";
+import { port } from "./port.js";
+import { link } from "./link.js";
+import { smooth } from "./smooth.js";
+
+window.addEventListener('load', function() {
+    menu();
+    link();
+    smooth();
+    port();
+})
